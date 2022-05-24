58f52fc4ca669d07a24e00849b7ff02c6035c554
jonathan1965 <jonakarisa@gmail.com>
2022-05-24 23:56:33 +0200

    make it better

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,56 @@
 import React from 'react'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Header/contact.css'
 function Aboute() {
   return (
-    <div>About</div>
+    <div class="contact_us">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 offset-md-1">
+                <div class="contact_inner">
+                    <div class="row">
+                        <div class="col-md-10">
+                            <div class="contact_form_inner">
+                                <div class="contact_field">
+                                    <h3>Get in Touch </h3>
+                                    <p>Feel Free to contact us any time. We will get back to you as soon as we can!.</p>
+                                    <input type="text" class="form-control form-group" placeholder="Name" />
+                                    <input type="text" class="form-control form-group" placeholder="Email" />
+                                    <textarea class="form-control form-group" placeholder="Message"></textarea>
+                                    <button class="contact_form_submit">Send</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-2">
+                            <div class="right_conatct_social_icon d-flex align-items-end">
+                               <div class="socil_item_inner d-flex">
+                                  <li><a href="#"><i className="fab fa-facebook-square"></i></a></li>
+                                  <li><a href="#"><i className="fab fa-instagram"></i></a></li>
+                                  <li><a href="#"><i className="fab fa-twitter"></i></a></li>
+                               </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="contact_info_sec">
+                        <h4>Contact Info</h4>
+                        <div class="d-flex info_single align-items-center">
+                            <i className="fas fa-headset"></i>
+                            <span>+250 788 393331</span>
+                        </div>
+                        <div class="d-flex info_single align-items-center">
+                            <i className="fas fa-envelope-open-text"></i>
+                            <span>jonathan@kalisa.com</span>
+                        </div>
+                        <div class="d-flex info_single align-items-center">
+                            <i className="fas fa-map-marked-alt"></i>
+                            <span>Sometimes it pays to stay in bed on Monday, rather than spending the rest of the week debugging Monday’s code.</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
   )
 }
 
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -24,7 +24,7 @@ function Homepage() {
       <div class="c-dashboardInfo col-lg-3 col-md-6">
         <div class="wrap">
           <h4 class="heading heading5 hind-font medium-font-weight c-dashboardInfo__title">Not Attended<svg
-              class="MuiSvgIcon-root-19" focusable="false" viewBox="0 0 24 24" aria-hidden="true" role="presentation">
+              class="MuiSvgIcon-root-19" focusable="false" viewBox="0 0 24 24" aria-hidden="true" >
               <path fill="none" d="M0 0h24v24H0z"></path>
               <path
                 d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z">
@@ -36,7 +36,7 @@ function Homepage() {
       <div class="c-dashboardInfo col-lg-3 col-md-6">
         <div class="wrap">
           <h4 class="heading heading5 hind-font medium-font-weight c-dashboardInfo__title">Total<svg
-              class="MuiSvgIcon-root-19" focusable="false" viewBox="0 0 24 24" aria-hidden="true" role="presentation">
+              class="MuiSvgIcon-root-19" focusable="false" viewBox="0 0 24 24" aria-hidden="true" >
               <path fill="none" d="M0 0h24v24H0z"></path>
               <path
                 d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z">
